eb84fa0d42ad238090cadf2375e8f75279898734
MAYANK SHARMA <sharmahost7@gmail.com>
2026-07-16 21:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import subprocess
+import sys
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,14 +17,37 @@ def home():
     )
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return "Register Student Page - Coming Next"
+    if request.method == "POST":
+        name = request.form.get("name")
+        student_id = request.form.get("student_id")
+        course = request.form.get("course")
+        semester = request.form.get("semester")
+
+        print("Name:", name)
+        print("Student ID:", student_id)
+        print("Course:", course)
+        print("Semester:", semester)
+
+        return f"Student {name} registered successfully!"
+
+    return render_template("register.html")
 
 
 @app.route("/attendance")
 def attendance():
-    return "Take Attendance Page - Coming Next"
+    subprocess.Popen([
+        sys.executable,
+        "src/recognize.py"
+    ])
+
+    return """
+    <h1>Face Recognition Started</h1>
+    <p>Camera should open in a separate window.</p>
+    <p>Press ESC in the camera window to stop attendance.</p>
+    <a href="/">Back to Dashboard</a>
+    """
 
 
 @app.route("/students")
